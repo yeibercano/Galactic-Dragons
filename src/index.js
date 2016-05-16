0f3f95cfcbb0c211004afa88ad643660b4ba0642
Aralya Phinith <aphinith84@gmail.com>
2016-05-16 02:57:29 -0700

    create src folder with index.js file for boiler plate

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        This is only the beginning!
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
