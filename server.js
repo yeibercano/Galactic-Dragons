ef902243a304449ca3e023997133b443cabe47dd
Aralya Phinith <aphinith84@gmail.com>
2016-05-16 17:21:40 -0700

    create new boiler plate to use express and webpack

--- server.js
+++ server.js
@@ -1,28 +1,38 @@
 var express = require('express');
+var path = require('path');
+var httpProxy = require('http-proxy');
+var publicPath = path.resolve(__dirname, 'public');
+
+// We need to add a configuration to our proxy server,
+// as we are now proxying outside localhost
+var isProduction = process.env.NODE_ENV === 'production';
+var port = isProduction ? process.env.PORT : 3000;
+
+var proxy = httpProxy.createProxyServer({
+  changeOrigin: true
+});
 var app = express();
-var bodyParser = require('body-parser');
 
-// PORT------------------------------------------
-var port = process.env.PORT || 8000;
+app.use(express.static(publicPath));
 
-app.use(bodyParser.json());
+// If you only want this for development, you would of course
+// put it in the "if" block below
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
-// app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname));
+if (!isProduction) {
+  var bundle = require('./server/compiler.js')
+  bundle()
+  app.all('/build/*', function (req, res) {
+    proxy.web(req, res, {
+        target: 'http://localhost:8080'
+    })
+  })
+};
 
-// routes ==================================================
+proxy.on('error', function(e) {
+  console.log('Could not connect to proxy, please try again...')
+});
 
-// require('./app/routes')(app); // configure our routes
-// app.use(express.static(__dirname + '/public'));
+app.listen(port, function () {
+  console.log('Server running on port ' + port)
+});
 
-// start app ===============================================
-// startup our app at http://localhost:8000
-app.listen(port);
-
-// shoutout to the user
-console.log('Listening in on port ' + port);
-
-// expose app
-exports = module.exports = app;
