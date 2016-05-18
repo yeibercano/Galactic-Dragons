220a093ed97a693701615d4a357c4a79e44b7df8
Aralya Phinith <aphinith84@gmail.com>
2016-05-17 19:10:41 -0700

    create front end upload button to connect with server

--- public/Components/test.js
+++ public/Components/test.js
@@ -1,14 +1,48 @@
 import React, { Component } from 'react'
+import 'isomorphic-fetch'
+
 
 class Test extends Component {
+
+  constructor (props) {
+  super(props);
+  this.state = { file: 'puppies' };
+  this._handleSubmit = this._handleSubmit.bind(this)
+  }
+
+
+  _handleSubmit(e) {
+  e.preventDefault();
+  let file = this.state.file;
+  // TODO: do something with -> this.state.file
+  // console.log('this is props inside handlesubmit:', this.props);
+  console.log('inside handlesubmit with the new state:', file);
+
+  fetch('/test', {
+    method: 'POST',
+    body: {data: file}
+  }, console.log('this is inside fetch post:', file))
+  .then(response =>{ console.log('this is response.json:', response.json()), response.json()})
+    .then(json => console.log('this is the undefined we were looking at earlier:', json))
+
+  // fetch('/test')
+  //   .then(response =>{console.log('this is response:', response), console.log('this is response.json:', response.json()), response.json()})
+  //   .then(json => console.log('this is the undefined we were looking at earlier:', json))
+  }
+
   render() {
     return (
-      <div>
-      <form action="#">
-        <input type="file"></input>
-        <input type="submit" onClick={console.log('yes')}></input>
-       </form> 
+
+       <div>
+          <form > 
+            <input
+            type="file"
+            value= {this.state.file}
+            onChange={event => this.setState({ file: event.target.value })} />
+            <input type="submit" onClick={this._handleSubmit} value="POST"/>
+          </form>
       </div>
+ 
     )
   }
 }
--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var express = require('express');
 var path = require('path');
 var httpProxy = require('http-proxy');
 var publicPath = path.resolve(__dirname, 'public');
-var streamingS3 = require('streaming-s3');
-var fs = require('fs');
-var secret = require('private.js');
+// var streamingS3 = require('streaming-s3');
+// var fs = require('fs');
+// var secret = require('private.js');
 
 // We need to add a configuration to our proxy server,
 // as we are now proxying outside localhost
@@ -18,20 +18,31 @@ var app = express();
 
 app.use(express.static(publicPath));
 
+app.get('/test', function(req, res) {
+  console.log('hi');
+  console.log(req.body);
+  res.json({});
+})
+
+app.post('/test', function(req, res) {
+  console.log('this is the post test!');
+  console.log('this is req.body:', req.body);
+})
+
 //Below code is for uploading file to S3 bucket in AWS using streaming-s3
 //'/video.mp4' should be replaced with variable that siginifies the mp4 being uploaded
-var fStream = fs.CreateReadStream(__dirname + '/video.mp4');
-var uploader = new streamingS3(fStream, {accessKeyId: 'accessKey', secretAccessKey: 'secretKey'},
-  {
-    Bucket: 'galactic.video',
-    Key: 'video.mp4',
-    ContentType: 'video/mp4'
-  }, function(err, resp, stats){
-    if(err) console.log("Upload error: ", e);
-    console.log("Upload stats: ", stats);
-    console.log("Upload successful", resp);
-  }
-);
+// var fStream = fs.CreateReadStream(__dirname + '/video.mp4');
+// var uploader = new streamingS3(fStream, {accessKeyId: 'accessKey', secretAccessKey: 'secretKey'},
+//   {
+//     Bucket: 'galactic.video',
+//     Key: 'video.mp4',
+//     ContentType: 'video/mp4'
+//   }, function(err, resp, stats){
+//     if(err) console.log("Upload error: ", e);
+//     console.log("Upload stats: ", stats);
+//     console.log("Upload successful", resp);
+//   }
+// );
 
 
 // If you only want this for development, you would of course
