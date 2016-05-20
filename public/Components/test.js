2cfdbaaf7205fdecd8e20a3f208b21e2fff55f8e
Aralya Phinith <aphinith84@gmail.com>
2016-05-20 16:23:41 -0700

    upload file from client side to s3 bucket complete

--- public/Components/test.js
+++ public/Components/test.js
@@ -1,72 +1,42 @@
 import React, { Component } from 'react'
-import 'isomorphic-fetch'
-
 
   class Test extends Component {
 
-    constructor (props) {
-    super(props);
-    // this.state = [];
-    this.state = { file: 'puppies' };
-    this._handleSubmit = this._handleSubmit.bind(this)
-    this.addFile = this.addFile.bind(this)
-    }
-
-///////////////////////////////////////////////////////////////////////
-  addFile(e){
-    console.log("This is e inside add file:", e)
-    this.setState({file: e.target.value})
-  }
+    //to handle our submit form
     _handleSubmit(e) {
     e.preventDefault();
 
+    //below function to send our form to our server
     var ajaxFileUpload = function(data) {
+    console.log('this is data inside ajaxFileUpload:', data);
     console.log('ajaxFileUpload Called!');
     var xhr = new XMLHttpRequest();
     xhr.open('POST', 'http://localhost:3000/api/testupload', true);
-    // xhr.setRequestHeader('Content-type', 'multipart/form-data;boundary=thisisarandomboundary');
     xhr.onload = function () {
-    // do something to response
     console.log(this.responseText);
-    };
-  xhr.send(data);
-  }
+    }
+    xhr.send(data);
+    }
 
-    console.log('handleSubmit event handler triggered');
-    console.log('this is this.state.file', this.state.file);
-    let newFile = this.state.file;
-    console.log('this is newFile:', newFile);
+    //below code to send our updated form to our ajaxFileUpload function
     var form = document.querySelector("form");
-    var fdata = new FormData(newFile);
+    console.log('this is form:', form);
+    var fdata = new FormData(form);
     console.log('this is fdata:', fdata);
-    // Optional. Append custom data.
-    // fdata.append('user_city', 'Los Angeles');
-
     ajaxFileUpload(fdata);
 
-    // Prevents the standard submit event
-    // return false;
-
-
-    console.log('this is form:', form);
-//////////////////////////////////////////////////////////////////////////
-
-
 }
-
-////////////////////////////////////////////////////////////////////////
 
     render() {
       return (
 
-         <div>
-            <form> 
+          <div>
+            <form onSubmit={this._handleSubmit}> 
               <input
-              type="file" name="file"
-              onChange={event=>this.addFile(event)}/>
-              <input type="submit" value="Upload Video" name="submit" onClick={this._handleSubmit}/>
+              type="file" name="file" />
+              <input type="submit" value="Upload Video" name="submit" />
             </form>
-        </div>
+          </div>
    
       )
     }
