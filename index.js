594ef1557d58813f773755fea845e5f0cedd3eaa
Dion <Dion.Fulwood@gmail.com>
2016-05-20 21:21:58 -0700

    Front-end register form

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 import Test from './public/Components/test'
+import CreateAccountScreen from './public/Components/register'
 
 class HelloWorld extends Component {
   render() {
     return (
       <div>Hello World!
         <Test/>
+        <CreateAccountScreen />
       </div>
 
     )
