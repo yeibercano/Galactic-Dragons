b4291e6ab2b3400734020b5ae0f31ca349b56fcf
Yeiber Cano <yeibercano@gmail.com>
2016-05-23 10:46:50 -0700

    start working on redirecting after login

--- server/routes/users.js
+++ server/routes/users.js
@@ -37,17 +37,50 @@ router.post('/register', function(req, res, next){
       if (err) throw err;
     
       console.log('user',user);
-      // console.log('req.params',res);
-      // res.redirect('https://slack.com/');
+      console.log('register calling home');
+      // res.status(200).json(user=user);
+      res.send({user:user})
+      .then(function(){
+        res.redirect('/')
+      })
   })
-  res.send('created user')
   // res.status(200);
 });
 
+// LOGIN //
+// router.get('/login', function(req, res, next) {
+//   // var loggedInUserID = req.cookies.userID;
+//   // console.log('get from routes.user')
+//   var query = [
+//     'MATCH (user:User)',
+//     'WHERE user.password = {password} and user.userName = {userName})',
+//     'RETURN user'
+//   ].join('\n');
+//   var params = {
+//     password: req.body.password,
+//     userName: req.body.userName 
+
+//   };
+  
+//   db.cypher({
+//     query: query,
+//     // params: params
+//   }, function(err, user){
+//     if (err) throw err;
+//     res.render('/', {title: 'Home', users: user});  
+//     // res.send({title: 'All Users', users: users, one:users.properties.name});  
+//     // res.send({title: 'All Users', user: user[user.length-1]});  
+//     console.log('user')
+//   });
+  
+// });
+
+
+//checks for user
 // QUERY ALL USERS //
 router.get('/all', function(req, res, next) {
   // var loggedInUserID = req.cookies.userID;
-  // console.log('get from routes.user')
+  console.log('req.cookies', req)
   var query = [
     'MATCH (users:User)',
     // 'WHERE NOT (ID(users) = {loggedInUserID})',
@@ -66,12 +99,39 @@ router.get('/all', function(req, res, next) {
     console.log('labels',user[user.length-1].users.labels)
     // res.render('users/index', {title: 'All Users', users: users});  
     // res.send({title: 'All Users', users: users, one:users.properties.name});  
-    res.send({title: 'All Users', user: user[user.length-1]});  
+    res.send({title: 'All Users', user: user});  
 
   });
   
 });
 
+router.get('/profile', function(req, res, next) {
+  // var loggedInUserID = req.cookies.userID;
+  console.log('req.cookies', req)
+  var query = [
+    'MATCH (users:User)',
+    // 'WHERE NOT (ID(users) = {loggedInUserID})',
+    'RETURN users'
+  ].join('\n');
+  // var params = {
+  //   loggedInUserID: loggedInUserID
+  // };
+  
+  db.cypher({
+    query: query,
+    // params: params
+  }, function(err, user){
+    if (err) throw err;
+    console.log('profile called')
+    // console.log('labels',user[user.length-1].users.labels)
+    // res.render('users/index', {title: 'All Users', users: users});  
+    // res.send({title: 'All Users', users: users, one:users.properties.name});  
+    // res.send({title: 'All Users', user: user[user.length-1]});  
+    res.send({user: user[user.length-1]});  
+
+  });
+  
+});
 
 
 module.exports = router;
