d2e603285c270beb8a4b45369e6018ee246e05e3
Yeiber Cano <yeibercano@gmail.com>
2016-05-23 10:42:02 -0700

    create component that holds user personal info

---
+++ public/Components/profileInfo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+var axios = require('axios');
+ 
+// shows ProfileInfo
+const ProfileInfo = React.createClass({
+  render() {
+    return (
+      <div>
+        <p>PROFILE INFO COMPONENT SHOWING</p>
+       
+      </div>
+    )
+  }
+})
+
+export default ProfileInfo
