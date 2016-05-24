3b7f215d27bc3aafb38f81b12a93481ca119977a
Yeiber Cano <yeibercano@gmail.com>
2016-05-24 08:40:09 -0700

    remove redirecting from register route, just serve new user

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var neo4j = require('neo4j');
 var secret = require('../../private.js');
 var db = new neo4j.GraphDatabase(secret.grapheneDB_URI);
+var newRouter = require('react-router')
 
 // CREATES NEW USERS
 router.post('/register', function(req, res, next){
@@ -38,16 +39,47 @@ router.post('/register', function(req, res, next){
     
       console.log('user',user);
       console.log('register calling home');
-      // res.status(200).json(user=user);
-      res.send({user:user})
-      .then(function(){
-        res.redirect('/')
-      })
+      res.status(200).json(user=user);
+      // res.redirect('/users/profile');
   })
   // res.status(200);
 });
 
-// LOGIN //
+/* LOADS PROFILE */
+router.get('/profile/:id', function(req, res, next) {
+  console.log('req', req)
+  console.log('res',res)
+  // var loggedInUserID = req.cookies.userID;
+  console.log('req.cookies', req)
+  var query = [
+    'MATCH (users:User)',
+    // 'WHERE NOT (ID(users) = {loggedInUserID})',
+    'RETURN users'
+  ].join('\n');
+  // var params = {
+  //   loggedInUserID: loggedInUserID
+  // };
+  
+  db.cypher({
+    query: query,
+    // params: params
+  }, function(err, user){
+    if (err) throw err;
+    console.log('profile called')
+    // console.log('labels',user[user.length-1].users.labels)
+    // res.render('users/index', {title: 'All Users', users: users});  
+    // res.send({title: 'All Users', users: users, one:users.properties.name});  
+    // res.send({title: 'All Users', user: user[user.length-1]});  
+    // console.log('user', user)
+    res.json({user: user[user.length-1]});
+    // res.redirect('/') 
+
+  });
+  
+});
+
+
+// QUERY ALL USERS //
 // router.get('/login', function(req, res, next) {
 //   // var loggedInUserID = req.cookies.userID;
 //   // console.log('get from routes.user')
@@ -75,63 +107,88 @@ router.post('/register', function(req, res, next){
   
 // });
 
-
-//checks for user
-// QUERY ALL USERS //
+/* QUERY ALL USERS */
 router.get('/all', function(req, res, next) {
-  // var loggedInUserID = req.cookies.userID;
-  console.log('req.cookies', req)
   var query = [
     'MATCH (users:User)',
-    // 'WHERE NOT (ID(users) = {loggedInUserID})',
     'RETURN users'
   ].join('\n');
-  // var params = {
-  //   loggedInUserID: loggedInUserID
-  // };
-  
+
   db.cypher({
     query: query,
-    // params: params
   }, function(err, user){
     if (err) throw err;
-    console.log('user')
-    console.log('labels',user[user.length-1].users.labels)
-    // res.render('users/index', {title: 'All Users', users: users});  
-    // res.send({title: 'All Users', users: users, one:users.properties.name});  
-    res.send({title: 'All Users', user: user});  
+    // res.send({title: 'All Users', users: user});  
+    // res.redirect('/')
+  // newRouter.browserHistory.push('/');
+  newRouter.transitionTo('/')
+  
 
   });
   
 });
 
-router.get('/profile', function(req, res, next) {
-  // var loggedInUserID = req.cookies.userID;
-  console.log('req.cookies', req)
-  var query = [
-    'MATCH (users:User)',
-    // 'WHERE NOT (ID(users) = {loggedInUserID})',
-    'RETURN users'
-  ].join('\n');
-  // var params = {
-  //   loggedInUserID: loggedInUserID
-  // };
-  
-  db.cypher({
-    query: query,
-    // params: params
-  }, function(err, user){
-    if (err) throw err;
-    console.log('profile called')
-    // console.log('labels',user[user.length-1].users.labels)
-    // res.render('users/index', {title: 'All Users', users: users});  
-    // res.send({title: 'All Users', users: users, one:users.properties.name});  
-    // res.send({title: 'All Users', user: user[user.length-1]});  
-    res.send({user: user[user.length-1]});  
 
-  });
+/* GET /users/login */
+router.get('/login', function(req, res, next){
   
+  // res.redirect('/users/login');
+  // res.status(200).send({redirect: '/'})
+  // // next();
+
+  // newRouter.transitionTo('/')
 });
 
+/* POST /users/login */
+router.post('/login', function(req, res, next){
+  // console.log('Login post')
+  console.log('req.body', req.body.userName)
+  var userName = req.body['userName'];
+  var password = req.body['password'];
+  
+  // // check if data is empty
+  // if (!email || !password){
+  //   // add a message
+  //   return res.redirect('/users/login');
+  // }
+  
+  var query = [
+    'MATCH (user:User { userName: {userName} })',
+    'RETURN user'
+  ].join('\n');
+  var params = {
+    userName: userName
+  }
+
+  db.cypher({
+    query: query,
+    params: params
+  }, 
+    function(err, user){
+      if (err) throw err;
+    
+      // console.log(user[0]['user']['properties']);
+      // if the user exists check if is active
+      console.log('user',user);
+      
+      // login the user
+      // var userID = user[0]['user']['_id'];
+      // var currentUser = user[0]['user']['properties'];
+      // res.cookie('userID', userID, {
+      //   domain: 'begin-imitate.codio.io',
+      //   maxAge: new Date(Date.now() + 9000)
+
+      // });
+      // res.cookie('user', currentUser, {
+      //   domain: 'begin-imitate.codio.io',
+      //   maxAge: new Date(Date.now() + 9000)
+      // });
+      res.json(user=user)
+     // res.send({redirect:'/'})
+     // res.redirect('/')
+     // newRouter.transitionTo('/')
+  });
+ 
+});
 
 module.exports = router;
