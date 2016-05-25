fc826e4511dc09e4d818742fe9697a687f21eebd
Yeiber Cano <yeibercano@gmail.com>
2016-05-25 14:17:40 -0700

    move s3 call to movies route

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.use(express.static(publicPath));
 
 var users = require('./server/routes/users');
 var movies = require('./server/routes/movies')
+app.use('/movies', movies)
 app.use('/users', users); /* POST user to db. */
 // app.use('/users', users); /* GET users listing. */
 // app.use('/*', function(req, res){
@@ -42,35 +43,35 @@ POST TO S3 BELOW
 //Below code is for uploading file to S3 bucket in AWS using streaming-s3
 //'/video.mp4' should be replaced with variable that siginifies the mp4 being uploaded
 
-var S3FS = require('s3fs');
-var s3fsImplementation = new S3FS('galactic.video',{
-  accessKeyId: secret.accessKey, 
-  secretAccessKey: secret.secretAccessKey,
-  endpoint : secret.endpointLocation
-});
+// var S3FS = require('s3fs');
+// var s3fsImplementation = new S3FS('galactic.video',{
+//   accessKeyId: secret.accessKey, 
+//   secretAccessKey: secret.secretAccessKey,
+//   endpoint : secret.endpointLocation
+// });
 
-// s3fsImplementation.create();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(mulitpartyMiddleware);
+// // s3fsImplementation.create();
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: true}));
+// app.use(mulitpartyMiddleware);
 
-app.post('/users/video', function(req, res){
-  var file = req.files.file;
-  console.log('this is file which is file:', file);
-  var stream = fs.createReadStream(file.path);
-  return s3fsImplementation.writeFile(file.originalFilename, stream)
-  .then(function(err){
-    fs.unlink(file.path, function(err){
-      if(err){
-        console.error("This is the error", err);
-      }
-      console.log('file upload success');
-    // console.log('this is res:', res);
-    })
-    res.send('File Upload Complete');
-  });
+// app.post('/users/video', function(req, res){
+//   var file = req.files.file;
+//   console.log('this is file which is file:', file);
+//   var stream = fs.createReadStream(file.path);
+//   return s3fsImplementation.writeFile(file.originalFilename, stream)
+//   .then(function(err){
+//     fs.unlink(file.path, function(err){
+//       if(err){
+//         console.error("This is the error", err);
+//       }
+//       console.log('file upload success');
+//     // console.log('this is res:', res);
+//     })
+//     res.send('File Upload Complete');
+//   });
 
-});
+// });
 
 /*
 ================================================================================================
