dfb8f2b3e4686873a82b816c7e819cb62d680076
Dion <Dion.Fulwood@gmail.com>
2016-05-25 16:10:15 -0700

    Place videoList as child component of videoPlayer

--- public/Components/profileVideoPlayer.js
+++ public/Components/profileVideoPlayer.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import ProfileVideoList from './profileVideoList'
 var secret = require("../../private.js")
 var axios = require('axios');
 
@@ -13,16 +14,21 @@ class ProfileVideoPlayer extends Component{
     console.log("For kicks: ", this.props.userInfo);
 
     this.state = {
-      url: ''
+      url: "https://s3-us-west-2.amazonaws.com/galactic.video/VideoForMarc.mp4",
+      allMovies: ["title1","title2","title3"]
     }
     //get request to db to retrieve the movie nodes and subsequent video url contained within
-    axios.get("users/movies", {params: {userName: this.props.userInfo.userName}})
-    .then(function(movieResponse){
-      //TODO: Get video url from movieResponse at very last index if it comes back in an array
-      //remember to put the component back into profile once finished with TODO
-      let movieUrl;
-      this.setState({url: movieUrl});
-    });
+    // axios.get("users/movies", {params: {userName: this.props.userInfo.userName}})
+    // .then(function(movieResponse){
+    //   TODO: Get video url from movieResponse at very last index if it comes back in an array
+    //   remember to put the component back into profile once finished with TODO. Place array of movies into allMovies property
+      
+   
+    // });
+      // let movieUrl = ;
+      // this.setState({url: movieUrl});
+      // this.setState({allMovies: })
+      // console.log("What is allMovies: ", this.state.allMovies);
 
   
   }
@@ -34,6 +40,10 @@ class ProfileVideoPlayer extends Component{
         <video controls>
             <source src={this.state.url} type="video/mp4" />
         </video>
+        <div>
+          <ProfileVideoList moviesList = {this.state.allMovies}/>
+        </div>
+
       </div>
     );
   }
