a26632c52f9d7da99d9198834eccac8003db964c
Dion <Dion.Fulwood@gmail.com>
2016-05-28 15:43:31 -0700

    Implement set State

--- public/Components/ViewingComponent/viewing.js
+++ public/Components/ViewingComponent/viewing.js
@@ -10,12 +10,15 @@ class ViewingPage extends Component {
 
   constructor (props) {
     super (props) 
-    
+    console.log('this is props in viewing.js:', props);
   }
 
- 
+ componentWillMount() {
+  console.log('this is props inside component will mount:', this.props);
+ }
 
   render() {
+    console.log('this is this.props inside render of viewing.js:', this.props);
     return (
       <div> 
         <h1> Viewing the Movies</h1>
