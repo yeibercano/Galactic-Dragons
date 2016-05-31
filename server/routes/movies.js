3186b963df8a12fe5f8c9145f0f04b7b6ed9b5a3
Yeiber Cano <yeibercano@gmail.com>
2016-05-31 13:59:17 -0700

    remove token authentication from all movies routes

--- server/routes/movies.js
+++ server/routes/movies.js
@@ -153,32 +153,32 @@ router.get('/user', function(req, res, next) {
 });
 
 
-/* ANY ROUTE BELOW THIS FUNCTION WILL BE AUTHENTICATED */
-router.use(function(req, res, next) {
-  console.log('req.query in mdidleware movies: ', req.body)
-  // check header or url parameters or post parameters for token
-  var token = req.body.token || req.query.token || req.headers['x-access-token'];
-  // decode token
-  if (token) {
-    // verifies secret and checks exp
-    jwt.verify(token, secret.jwtSecret, function(err, decoded) {      
-      if (err) {
-        return res.json({ success: false, message: 'Failed to authenticate token.' });    
-      } else {
-        // if everything is good, save to request for use in other routes
-        req.decoded = decoded;
-        next();
-      }
-    });
-  } else {
-    // if there is no token
-    // return an error
-    return res.status(403).send({
-        success: false,
-        message: 'No token provided.'
-    });
-  }
-});
+// /* ANY ROUTE BELOW THIS FUNCTION WILL BE AUTHENTICATED */
+// router.use(function(req, res, next) {
+//   console.log('req.query in mdidleware movies: ', req.body)
+//   // check header or url parameters or post parameters for token
+//   var token = req.body.token || req.query.token || req.headers['x-access-token'];
+//   // decode token
+//   if (token) {
+//     // verifies secret and checks exp
+//     jwt.verify(token, secret.jwtSecret, function(err, decoded) {      
+//       if (err) {
+//         return res.json({ success: false, message: 'Failed to authenticate token.' });    
+//       } else {
+//         // if everything is good, save to request for use in other routes
+//         req.decoded = decoded;
+//         next();
+//       }
+//     });
+//   } else {
+//     // if there is no token
+//     // return an error
+//     return res.status(403).send({
+//         success: false,
+//         message: 'No token provided.'
+//     });
+//   }
+// });
 
 
 
