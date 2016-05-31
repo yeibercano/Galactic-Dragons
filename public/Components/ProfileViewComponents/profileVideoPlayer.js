76ccc61f00ba774786c5af6624395647ac0714f8
Dion <Dion.Fulwood@gmail.com>
2016-05-31 12:14:05 -0700

    Up to date

--- public/Components/ProfileViewComponents/profileVideoPlayer.js
+++ public/Components/ProfileViewComponents/profileVideoPlayer.js
@@ -2,47 +2,34 @@ import React, {Component} from 'react'
 import ProfileVideoList from './profileVideoList'
 var secret = require("../../../private.js")
 var axios = require('axios');
-
 class ProfileVideoPlayer extends React.Component {
-
   constructor (props) {
     super (props)  
     this.state = {
       url: "",
-      allMovies: this.props.allMovies
+      allMovies: ""
     }
    }
-
-
   componentWillMount() {
     axios.get("/movies/user", {params: {userName: this.props.userInfo.userName}}).then(data => {
       this.setState({ url: data.data[data.data.length-1].m.properties.video, allMovies: data.data });
     });
   }
-
-
   selectedMovie (movie) {
     console.log('A new movie was selected!', movie.video);
     this.setState({url: movie.video})
   }
-
   render() {
-   if(this.props.allMovies === ""){
-    return <div>loading... </div>
-   }
-    console.log('this.props profilve video Player', this.props)
     return (
       <section className="profile_video_player">
         <h1>This is The Users Most Recent Upload</h1>
-        <video controls src={this.props.allMovies[this.props.allMovies.length-1].m.properties.video} type="video/mp4" />
+        <video controls src={this.state.url} type="video/mp4" />
          <ProfileVideoList 
           selectedMovie = {(selectedMovie) => this.selectedMovie(selectedMovie)}
-          moviesList = {this.props.allMovies}
+          moviesList = {this.state.allMovies}
          />
       </section>
     );
   }
 }
-
 export default ProfileVideoPlayer
-
