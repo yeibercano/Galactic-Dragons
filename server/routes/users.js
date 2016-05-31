3569e98b280651940bd94a72e437758a4c7f4df2
Yeiber Cano <yeibercano@gmail.com>
2016-05-31 13:56:36 -0700

    remove token decode from all users route

--- server/routes/users.js
+++ server/routes/users.js
@@ -183,33 +183,33 @@ router.post('/login', function(req, res, next){
 });
 
 /* ANY ROUTE BELOW THIS FUNCTION WILL BE AUTHENTICATED */
-router.use(function(req, res, next) {
-  console.log('req. query in middleware:', req.body)
-  console.log('req. query in middleware:', req.headers)
-  console.log('req. query in middleware:', req.query)
-  // check header or url parameters or post parameters for token
-  var token = req.body.token || req.query.token || req.headers['x-access-token'];
-  // decode token
-  if (token) {
-    // verifies secret and checks exp
-    jwt.verify(token, secret.jwtSecret, function(err, decoded) {      
-      if (err) {
-        return res.json({ success: false, message: 'Failed to authenticate token.' });    
-      } else {
-        // if everything is good, save to request for use in other routes
-        req.decoded = decoded;    
-        next();
-      }
-    });
-  } else {
-    // if there is no token
-    // return an error
-    return res.status(403).send({ 
-        success: false, 
-        message: 'No token provided.' 
-    });
-  }
-});
+// router.use(function(req, res, next) {
+//   console.log('req. query in middleware:', req.body)
+//   console.log('req. query in middleware:', req.headers)
+//   console.log('req. query in middleware:', req.query)
+//   // check header or url parameters or post parameters for token
+//   var token = req.body.token || req.query.token || req.headers['x-access-token'];
+//   // decode token
+//   if (token) {
+//     // verifies secret and checks exp
+//     jwt.verify(token, secret.jwtSecret, function(err, decoded) {      
+//       if (err) {
+//         return res.json({ success: false, message: 'Failed to authenticate token.' });    
+//       } else {
+//         // if everything is good, save to request for use in other routes
+//         req.decoded = decoded;    
+//         next();
+//       }
+//     });
+//   } else {
+//     // if there is no token
+//     // return an error
+//     return res.status(403).send({ 
+//         success: false, 
+//         message: 'No token provided.' 
+//     });
+//   }
+// });
 
 /* QUERY ALL USERS */
 router.get('/all', function(req, res, next) {
