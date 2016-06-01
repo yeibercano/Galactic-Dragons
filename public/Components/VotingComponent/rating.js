45765d4defa3e3a7b9b0c18f0ad4fc1ad8c0072d
Yeiber Cano <yeibercano@gmail.com>
2016-06-01 15:53:33 -0700

    add post request with rating, username, view

--- public/Components/VotingComponent/rating.js
+++ public/Components/VotingComponent/rating.js
@@ -1,20 +1,35 @@
 import React from 'react';
 import ReactStars from 'react-stars'
+var axios = require('axios');
  
 class Rating extends React.Component {
   constructor() {
       super();
+      let movieSelected = localStorage.getItem('viewerMovie')
+      console.log('this is movieSelected:', movieSelected)
+      movieSelected = JSON.parse(movieSelected);
+      let user = localStorage.getItem('user')
+      user = JSON.parse(user);
+
       this.state = {
-          rating: 1
+          rating: 1, 
+          title: movieSelected.title, 
+          voter: user.userName
       };
+      // console.log('movieSelected', movieSelected)
       this.ratingChanged = this.ratingChanged.bind(this);
   }
   ratingChanged(vote) {
       this.setState({rating: vote}, function(){  
       alert("This will be your only opportunity to vote");
-      console.log("What is this.state.rating", this.state.rating);
+      // console.log("What is this.state.rating", this.state.rating);
+      // console.log('movieSelected', this.movieSelected)
       
     });
+    axios.post('/movies/rating', { rating: this.state.rating, title: this.state.title, voter: this.state.voter })
+    .then(function(res){
+      console.log('res', res)
+    })
 
   }
   render() {
