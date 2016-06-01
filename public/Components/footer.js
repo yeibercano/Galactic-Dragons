20d56a94e02ff37642bae8cedd0eaf07b4a335a7
Aralya Phinith <aphinith84@gmail.com>
2016-05-31 19:16:03 -0700

    update footer to have sovereign in it

--- public/Components/footer.js
+++ public/Components/footer.js
@@ -9,7 +9,7 @@ class Footer extends Component {
   render() {
     return (
       <footer> 
-        <h3>This is the footer</h3>
+        <h3>Sovereign &copy; 2016</h3>
       </footer>
     )
   }
