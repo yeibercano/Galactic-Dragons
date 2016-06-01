7794a00c02e21948f2924df2e064fbcaa791ec2a
Dion <Dion.Fulwood@gmail.com>
2016-05-31 19:33:35 -0700

    Set up rating system

--- public/Components/ProfileViewComponents/uploadNewVideo.js
+++ public/Components/ProfileViewComponents/uploadNewVideo.js
@@ -31,6 +31,9 @@ var UploadNewVideo = React.createClass({
       userName : parseUser.userName,
       video: secret.endpointLocation + '/' + secret.bucketName + '/' + videoFile,
       image : secret.endpointLocation + '/' + secret.bucketName + '/' + imageFile, 
+      plays: 0,
+      rating: 0,
+      voters: [],
       token : token
     }
     
--- public/Components/VotingComponent/rating.js
+++ public/Components/VotingComponent/rating.js
@@ -7,9 +7,15 @@ class Rating extends React.Component {
       this.state = {
           rating: 1
       };
+      this.ratingChanged = this.ratingChanged.bind(this);
   }
-  ratingChanged(name, value) {
-      this.setState({rating: value});
+  ratingChanged(vote) {
+      this.setState({rating: vote}, function(){  
+      alert("This will be your only opportunity to vote");
+      console.log("What is this.state.rating", this.state.rating);
+      
+    });
+
   }
   render() {
     const { rating } = this.state;
@@ -18,7 +24,7 @@ class Rating extends React.Component {
         <h2>Rating from state: {rating}</h2>
         <ReactStars
           count={5}
-          onChange={this.ratingChanged}
+          onChange={vote=>this.ratingChanged(vote)}
           size={24} 
           color2={'#ffd700'} />
       </div>
