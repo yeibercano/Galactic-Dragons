f2a03da81868a1f783286f320c0f515c93fcd4b7
Aralya Phinith <aphinith84@gmail.com>
2016-06-03 15:39:22 -0700

    create profile link

---
+++ public/Components/Header/userProfile.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+//this component is for the Logo/Brand
+class UserProfile extends Component {
+ 
+  render() {
+    return (
+      <aside class="userLogin"> 
+        <h1>Profile</h1>
+      </aside>
+    )
+  }
+}
+
+export default UserProfile
