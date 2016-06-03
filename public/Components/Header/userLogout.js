e1504077b174bc825edac6e2b2f6a255ad818511
Aralya Phinith <aphinith84@gmail.com>
2016-06-03 15:38:46 -0700

    create user logout link

---
+++ public/Components/Header/userLogout.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+//this component is for the Logo/Brand
+class UserLogout extends Component {
+ 
+  render() {
+    return (
+      <aside class="userLogin"> 
+        <h1>Logout</h1>
+      </aside>
+    )
+  }
+}
+
+export default UserLogout
