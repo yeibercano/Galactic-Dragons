1d1d3a6fc05d15f235a7bd9dba57e3e05c51f2ca
Aralya Phinith <aphinith84@gmail.com>
2016-06-03 15:40:42 -0700

    create search bar and functionality to capture value when typed in by user

--- public/Components/Header/SearchBar.js
+++ public/Components/Header/SearchBar.js
@@ -1,12 +1,42 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
 
 //this component is for the Logo/Brand
 class SearchBar extends Component {
+
+  searchTerm (e) {
+    let searchedItem = e;
+    console.log(searchedItem)
+    // this._submitSearch = _submitSearch.bind(this);
+    // localStorage.setItem('movieSearched', JSON.stringify(searchedItem));
+    localStorage.setItem('searchTerm', searchedItem);
+  }
+
+  submitSearch () {
+    let searchedItem = localStorage.getItem('searchTerm');
+    axios.get('/movies/search', {params: {target: searchedItem}})
+    .then(data => {
+        localStorage.setItem('searchResults', JSON.stringify(data.data));
+      })
+    .then(function() {
+    hashHistory.push('/search');
+    })
+    .catch(function(err) {
+      if (err) throw err
+    })
+  }
  
   render() {
     return (
       <aside class="searchBar"> 
-        <h1>Search Bar</h1>
+        <form>
+          <input onChange={event => this.searchTerm(event.target.value)} />
+          <button 
+            onClick= {this.submitSearch}
+            type="submit"
+            className="search_button"
+            >Search</button>
+        </form>
       </aside>
     )
   }
