8c4b232bb09f28ea9d8b307a9d31146c1c7b4ad8
Yeiber Cano <yeibercano@gmail.com>
2016-06-03 12:20:50 -0700

    header components and its children

---
+++ public/Components/Header/Nav.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+//this component is for the Logo/Brand
+class Nav extends Component {
+ 
+  render() {
+    return (
+      <nav class="brand"> 
+        <h1>Menu - ul</h1>
+      </nav>
+    )
+  }
+}
+
+export default Nav
